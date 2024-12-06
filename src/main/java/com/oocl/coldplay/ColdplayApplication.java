a8a900568d9d5330be1ad04fdeeeb2c70e8a023c
HEAL <HEAL@OOCL.COM>
2024-12-06 16:42:10 +0800

    [Coldplay] chore: initialize project

---
+++ src/main/java/com/oocl/coldplay/ColdplayApplication.java
@@ -0,0 +1,12 @@
+package com.oocl.coldplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColdplayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ColdplayApplication.class, args);
+    }
+}
