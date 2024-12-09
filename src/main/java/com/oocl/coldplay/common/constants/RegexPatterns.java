55265e4e5342c2310d1c78937101ba0184e5acda
HEAL <HEAL@OOCL.COM>
2024-12-09 16:26:24 +0800

    [Coldplay] feat: add RedisCacheService and RegexUtils

---
+++ src/main/java/com/oocl/coldplay/common/constants/RegexPatterns.java
@@ -0,0 +1,8 @@
+package com.oocl.coldplay.common.constants;
+
+public abstract class RegexPatterns {
+    /**
+     * 邮箱正则
+     */
+    public static final String EMAIL_REGEX = "^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\\.[a-zA-Z0-9_-]+)+$";
+}
